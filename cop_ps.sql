fa501e6abaa3840240a74334fe34449d396c2565
albertounivr <34159167+albertounivr@users.noreply.github.com>
2019-12-20 15:37:48 +0100

    Create cop_ps.sql

---
+++ cop_ps.sql
@@ -0,0 +1,100 @@
+CREATE OR REPLACE FUNCTION cop_ps(geometry, geometry)
+  RETURNS boolean AS
+$BODY$
+DECLARE
+g1 geometry;
+g2 geometry;
+pa geometry;
+s geometry;
+pt1 geometry;
+pt2 geometry;
+pt3 geometry;
+pt geometry;
+A double precision;
+A1 double precision;
+A2 double precision;
+A3 double precision;
+prec double precision;
+isPlanar boolean;
+BEGIN
+prec := 0.01;
+g1 := $1;
+g2 := $2;
+
+IF g1 IS NULL OR ST_ISEMPTY(g1) OR g2 IS NULL OR ST_ISEMPTY(g2) THEN
+  RETURN false;
+END IF;
+
+IF (GEOMETRYTYPE(g1) = 'POLYGON' AND GEOMETRYTYPE(g2) = 'LINESTRING' AND ST_NUMPOINTS(g2) = 2) THEN
+   IF ST_NDIMS(g1) <> 3 OR ST_NDIMS(g2) <> 3 THEN
+      RAISE NOTICE 'Input geometries are not 3D';
+      RETURN false;
+   END IF;
+   RAISE NOTICE 'COPLANAR between a patch and a segment in 3D';
+   pa := g1;
+   s := g2;
+   RAISE NOTICE 'Patch: %', ST_ASEWKT(pa);
+   RAISE NOTICE 'Segment: %', ST_ASEWKT(s);
+   
+   -- Testing if pa is planar
+   pt1 := ST_PointN(ST_EXTERIORRING(pa),1);
+   --RAISE NOTICE 'Pt1: %', ST_ASEWKT(pt1);
+   pt2 := ST_PointN(ST_EXTERIORRING(pa),2);
+   --RAISE NOTICE 'Pt2: %', ST_ASEWKT(pt2);
+   pt3 := ST_PointN(ST_EXTERIORRING(pa),3);
+   --RAISE NOTICE 'Pt3: %', ST_ASEWKT(pt3);
+   -- apply the Cramer's method for computing the coefficients A1 A2 and A3 of the plane that passes throuth the points p1, p2 and p3
+   -- A is determinat of the matrix of coordinates and represents the denominator or the known term
+   -- like A1 * x + A2 * y +A3 * z = A that derives from multiplying for A the following equation:
+   -- A1/A * x + A2/A * y + A3/A * z = 1
+   A := ST_X(pt1)*(ST_Y(pt2)*ST_Z(pt3) - ST_Y(pt3)*ST_Z(pt2)) - 
+		ST_X(pt2)*(ST_Y(pt1)*ST_Z(pt3) - ST_Y(pt3)*ST_Z(pt1)) + 
+		ST_X(pt3)*(ST_Y(pt1)*ST_Z(pt2) - ST_Y(pt2)*ST_Z(pt1));
+   -- A1 is the determinat of the matrix of coordinates where the known term (1 1 1) is substituted to the column of x										
+   A1 := (ST_Y(pt2)*ST_Z(pt3) - ST_Y(pt3)*ST_Z(pt2)) - 
+		 (ST_Y(pt1)*ST_Z(pt3) - ST_Y(pt3)*ST_Z(pt1)) +
+		 (ST_Y(pt1)*ST_Z(pt2) - ST_Y(pt2)*ST_Z(pt1));   
+   -- A2 is the determinat of the matrix of coordinates where the known term (1 1 1) is substituted to the column of y	
+   A2 := ST_X(pt1)*(ST_Z(pt3) - ST_Z(pt2)) -
+	     ST_X(pt2)*(ST_Z(pt3) - ST_Z(pt1)) +
+		 ST_X(pt3)*(ST_Z(pt2) - ST_Z(pt1));
+   -- A3 is the determinat of the matrix of coordinates where the known term (1 1 1) is substituted to the column of z	
+   A3 := ST_X(pt1)*(ST_Y(pt2) - ST_Y(pt3)) -
+	     ST_X(pt2)*(ST_Y(pt1) - ST_Y(pt3)) +
+	     ST_X(pt3)*(ST_Y(pt1) - ST_Y(pt2));
+   isPlanar := true;
+   
+   FOR pt IN SELECT geom FROM ST_DUMPPOINTS(pa) LOOP
+		--RAISE NOTICE 'Pti: %', ST_ASEWKT(pt);			 
+   		IF (ST_EQUALS(pt1,pt) OR ST_EQUALS(pt2,pt) OR ST_EQUALS(pt3,pt)) THEN
+		   --RAISE NOTICE 'same point: %', ST_ASEWKT(pt);
+		   CONTINUE;
+		END IF;
+        -- test coplanarity
+	    --RAISE NOTICE 'A: %, A1: %, A2: %, A3: % val: %', A, A1, A2, A3, abs((ST_X(pt)*A1 + ST_Y(pt)*A2 + ST_Z(pt)*A3) - A);            
+	    IF abs((ST_X(pt)*A1 + ST_Y(pt)*A2 + ST_Z(pt)*A3) - A) < prec THEN     
+		   CONTINUE;
+	    ELSE
+		   RAISE NOTICE 'Input 3D polygon is not planar';
+		   RETURN false;
+	    END IF;					   
+   END LOOP;
+   pt := ST_PointN(s,1);
+   --RAISE NOTICE 'Pt1-s: %', ST_ASEWKT(pt);								 
+   IF abs((ST_X(pt)*A1 + ST_Y(pt)*A2 + ST_Z(pt)*A3) - A) > prec THEN  
+	  RETURN false;
+   END IF;
+   pt := ST_PointN(s,2);
+   --RAISE NOTICE 'Pt2-s: %', ST_ASEWKT(pt);	
+   IF abs((ST_X(pt)*A1 + ST_Y(pt)*A2 + ST_Z(pt)*A3) - A) > prec THEN  
+	  RETURN false;
+   END IF;
+   RETURN true;
+ELSE								 
+   RAISE NOTICE 'Input geometries are not a patch and a segment';
+   RETURN false;
+END IF;   
+END;
+$BODY$
+  LANGUAGE 'plpgsql' VOLATILE
+  COST 100;
